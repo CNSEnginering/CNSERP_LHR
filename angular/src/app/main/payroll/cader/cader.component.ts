3e33fe166ffe44b38fb692dc0201dddf13c3796b
BassamMahboob <bassam.ahmed1810@gmail.com>
2022-09-28 12:17:39 +0500

    bassam first commit

--- angular/src/app/main/payroll/cader/cader.component.ts
+++ angular/src/app/main/payroll/cader/cader.component.ts
@@ -12,7 +12,7 @@ import { AppComponentBase } from '@shared/common/app-component-base';
 import { caderDTo } from '../shared/dto/Cader-dto';
 import { CreateOrEditCaderComponent } from './createoreditcader.component';
 
-//testing git branch 2nd time
+//testing git branch 3rd time
 @Component({
   templateUrl: './cader.component.html',
   encapsulation: ViewEncapsulation.None,
